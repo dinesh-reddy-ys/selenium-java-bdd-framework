7454397cd1acfa85654249009253c4ab17263d00
dinesh-reddy-ys <dineshxyz07@gmail.com>
2025-08-06 11:37:38 +0530

    code formatting

--- src/main/java/pages/WebTablesPage.java
+++ src/main/java/pages/WebTablesPage.java
@@ -12,56 +12,55 @@ import java.time.Duration;
 import java.util.*;
 
 public class WebTablesPage implements IWebTablesPages {
-    public WebDriver driver;
-    public WebDriverWait wait;
+	public WebDriver driver;
+	public WebDriverWait wait;
 
-    @FindBy(id = "addNewRecordButton")
-    private WebElement addButton;
+	@FindBy(id = "addNewRecordButton")
+	private WebElement addButton;
 
-    @FindBy(id = "searchBox")
-    private WebElement searchBox;
+	@FindBy(id = "searchBox")
+	private WebElement searchBox;
 
-    @FindBy(css = "div.rt-tr-group")
-    private List<WebElement> rowLocator;
+	@FindBy(css = "div.rt-tr-group")
+	private List<WebElement> rowLocator;
 
-    @FindBy(css = "div.rt-td")
-    private List<WebElement> columnLocator;
+	@FindBy(css = "div.rt-td")
+	private List<WebElement> columnLocator;
 
-    // Table column locator as reusable By object (not hardcoded in method!)
-    private final By columnBy = By.cssSelector("div.rt-td");
+	// Table column locator as reusable By object (not hardcoded in method!)
+	private final By columnBy = By.cssSelector("div.rt-td");
 
+	public WebTablesPage(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		PageFactory.initElements(driver, this);
+	}
 
-    public WebTablesPage(WebDriver driver){
-        this.driver = driver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        PageFactory.initElements(driver,this);
-    }
+	public void clickOnAddButton() {
+		System.out.println("hello");
+	}
 
-    public void clickOnAddButton(){
-        System.out.println("hello");
-    }
-    public void search(String searchText){
-        System.out.println(searchText);
-    }
+	public void search(String searchText) {
+		System.out.println(searchText);
+	}
 
-    // Inside method: you can loop through rows and get columns from each row
-    public List<List<String>> getWebTableData() {
-        List<List<String>> tableData = new ArrayList<>();
+	// Inside method: you can loop through rows and get columns from each row
+	public List<List<String>> getWebTableData() {
+		List<List<String>> tableData = new ArrayList<>();
 
-        for (WebElement row : rowLocator) {
-            // Columns inside each row
-            List<WebElement> columns = row.findElements(columnBy);
-            List<String> rowData = new ArrayList<>();
+		for (WebElement row : rowLocator) {
+			// Columns inside each row
+			List<WebElement> columns = row.findElements(columnBy);
+			List<String> rowData = new ArrayList<>();
 
-            for (WebElement column : columns) {
-                rowData.add(column.getText());
-            }
+			for (WebElement column : columns) {
+				rowData.add(column.getText());
+			}
 
-            tableData.add(rowData);
-        }
+			tableData.add(rowData);
+		}
 
-        return tableData;
-    }
-
+		return tableData;
+	}
 
 }
--- src/test/java/stepDefs/UploadAndDownload.java
+++ src/test/java/stepDefs/UploadAndDownload.java
@@ -23,26 +23,27 @@ public class UploadAndDownload {
 	WebDriver driver;
 	UploadAndDownloadPage uploadAndDownloadPage;
 	TabsAndDropdownsPage tabsAndDropdownsPage;
-	DownloadUtils downloadUtils ;
+	DownloadUtils downloadUtils;
 	static {
-		System.setProperty("logback.configurationFile","logback.xml");
+		System.setProperty("logback.configurationFile", "logback.xml");
 	}
-	
+
 	public UploadAndDownload() {
 		this.driver = DriverFactory.getDriver();
 		uploadAndDownloadPage = new UploadAndDownloadPage(driver);
 		tabsAndDropdownsPage = new TabsAndDropdownsPage(driver);
 		test = ExtentManager.createTest("Upload and Download Test");
-		downloadUtils = new DownloadUtils(System.getProperty("user.home")+ "/Downloads");
+		downloadUtils = new DownloadUtils(System.getProperty("user.home") + "/Downloads");
 	}
-	
+
 	@Given("I want to navigate to download and upload page")
 	public void i_navigate_to_the_upload_and_download_page() {
-		//driver.get(url);
+		// driver.get(url);
 		uploadAndDownloadPage.clickOnElementsDropDown();
 		uploadAndDownloadPage.clickOnUploadAndDownloadTab();
 		test.info("Navigated to the Upload and Download page: ");
 	}
+
 	@When("I click on the download button")
 	public void i_click_on_the_download_button() {
 		uploadAndDownloadPage.scrollToDownloadButton();
@@ -50,29 +51,22 @@ public class UploadAndDownload {
 		test.info("Cleared the download folder before downloading the file.");
 		uploadAndDownloadPage.clickDownloadButton();
 		test.info("Clicked on the download button.");
-		
-		
+
 	}
+
 	@Then("I verify the file is downloaded successfully")
 	public void verify_the_file_is_downloaded_successfully() {
 		// This step is intentionally left blank.
 		// The file download verification will be handled in the hooks.
-		boolean  isDownloaded = downloadUtils.waitForFileToDownload("sampleFile.jpeg", 15);
+		boolean isDownloaded = downloadUtils.waitForFileToDownload("sampleFile.jpeg", 15);
 		Assert.assertTrue(isDownloaded, "File was not downloaded successfully!");
 		test.info("File downloaded successfully and verified.");
 		System.out.println("File download verification will be handled in the hooks.");
 	}
-//	@Given("I navigate to {string}")
-//	public void i_navigate_to(String url) {
-//		driver.get(url);
-//		driver.manage().window().maximize();
-//		test.info("Navigated to the URL: " + url);
-//		logger.info("Navigated to the URL: " + url);
-//	}
-	
+
 	@When("I upload the file with path {string}")
 	public void i_upload_a_file_with_path(String filePath) {
-		//uploadAndDownloadPage.scrollToUploadFile();
+		// uploadAndDownloadPage.scrollToUploadFile();
 		uploadAndDownloadPage.scrollToDownloadButton();
 		uploadAndDownloadPage.uploadFile(filePath);
 		try {
@@ -84,23 +78,23 @@ public class UploadAndDownload {
 		test.info("Uploaded file with path:" + filePath);
 		logger.info("Uploaded file with path: " + filePath);
 	}
+
 	@Then("I Verify the uploaded file path is displayed")
 	public void verify_the_uploaded_file_path_is_displayed() {
 		boolean isDisplayed = uploadAndDownloadPage.uploadedFilepath.isDisplayed();
 		Assert.assertTrue(isDisplayed, "uplaoded file path is not displyed!");
 		test.info("Uploaded file path is displed successfully: " + uploadAndDownloadPage.uploadedFilepath.getText());
-	    logger.info("Uploaded file path is displyed successfully: " + uploadAndDownloadPage.uploadedFilepath.getText());
+		logger.info("Uploaded file path is displyed successfully: " + uploadAndDownloadPage.uploadedFilepath.getText());
 	}
 
 	@Given("I want to click on elements dropdown")
-	public void i_want_to_click_on_elements_dropdown(){
+	public void i_want_to_click_on_elements_dropdown() {
 		tabsAndDropdownsPage.clickOnElementsDropdown();
 	}
 
 	@And("I want to select upload and download tab")
-	public void i_want_to_select_upload_and_download_tab(){
+	public void i_want_to_select_upload_and_download_tab() {
 		tabsAndDropdownsPage.clickOnUploadAndDownloadTab();
 	}
-
 
 }
