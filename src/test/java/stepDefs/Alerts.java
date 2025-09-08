e0909dd88bd36d5a38ba45b7178e13a3f2abccb7
dinesh reddy <100275007+dinesh-reddy-ys@users.noreply.github.com>
2025-09-08 15:30:14 +0530

    refactor alerts (#53)

--- src/test/java/stepDefs/Alerts.java
+++ src/test/java/stepDefs/Alerts.java
@@ -8,22 +8,16 @@ public class Alerts {
 
 	@Given("I launch the demoqa url")
 	public void iWantToSeeAnAlert() {
-		// This step is intentionally left blank.
-		// The alert will be handled in the hooks.
 		System.out.println("Alert is expected to be shown.");
 	}
 	
 	@When("Click on the alert button")
 	public void iClickOnTheAlertButton() {
-		// This step is intentionally left blank.
-		// The alert button click will be handled in the hooks.
 		System.out.println("Alert button clicked.");
 	}
 	
 	@Then("Accept the alert")
 	public void iShouldSeeAnAlertWithTheMessage() {
-		// This step is intentionally left blank.
-		// The alert verification will be handled in the hooks.
 		System.out.println("Alert message verified: ");
 	}
 
