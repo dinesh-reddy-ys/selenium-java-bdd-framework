2078258b4e9be767460befb2a51a4182748e29eb
dinesh reddy <100275007+dinesh-reddy-ys@users.noreply.github.com>
2025-09-08 11:34:56 +0530

    code cleanup in file SerachItem.java (#52)

--- src/test/java/stepDefs/SearchItem.java
+++ src/test/java/stepDefs/SearchItem.java
@@ -4,22 +4,17 @@ import pages.HomePage;
 import utils.DriverFactory;
 import utils.ExcelUtil;
 
-import org.slf4j.LoggerFactory;
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
 import java.util.List;
 import java.util.Map;
 
 public class SearchItem {
 
-    private static final Logger logger = LoggerFactory.getLogger(SearchItem.class);
     public List<Map<String, String>> searchItem = ExcelUtil.readExcelData("src/test/resources/testData/TestData.xlsx");
     public WebDriver driver;
     public HomePage homePage;
     private List<Map<String, String>> testData;
     private static final String EXCEL_PATH = "src/test/resources/testData/TestData.xlsx";
-    private String username;
-    private String password;
     static {
         System.setProperty("logback.configurationFile", "logback.xml");
     }
@@ -27,12 +22,12 @@ public class SearchItem {
     public SearchItem() {
         this.driver = DriverFactory.getDriver();
         this.homePage = new HomePage(driver);
-        this.username = System.getProperty("username");
-        this.password = System.getProperty("password");
-        // Load test data from Excel
+        System.getProperty("username");
+        System.getProperty("password");
         loadTestData();
     }
-
+    
+    // Method to load test data from Excel
     private void loadTestData() {
         try {
             testData = ExcelUtil.readExcelData(EXCEL_PATH);
